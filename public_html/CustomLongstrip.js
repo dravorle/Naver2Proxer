d7b5b40a31dd2457b2e8f79018992c842ea4ffb5
Dravorle <dravorle@gmx.de>
2018-04-10 13:03:23 +0200

    First Release
    First Release

---
+++ public_html/CustomLongstrip.js
@@ -0,0 +1,244 @@
+var isSpread = false;
+var scrollable = false;
+var scroll = true;
+var counter = 0;
+var gt_key_suggestion_long = 'Verwende W/S bzw. Pfeil oben/unten zum scrollen.<br>' +
+        'Doppelklicke auf A/D bzw. Pfeil links/rechts um das Kapitel zu wechseln.<br>' +
+        'DrÃ¼cke am Ende eines Kapitels D/Pfeil rechts, um das nÃ¤chste Kapitel zu erreichen.';
+
+function initLong() {
+    $("#readerLink").on("click", function() {
+        nextPageLong(); return false;
+    });
+
+    $('#pages').html('<div class="number loaded"><a href="javascript:void(0);" >' + pages.length + '</a></a>');
+    var list = '<ul id="listPages">';
+    var images = '';
+    for (i = 0; i < pages.length; i++) {
+        list += '<li><a href="javascript:;" onClick="changePageLong(' + (i + 1) + ',false); return false;">Page ' + (i + 1) + '</a></li>';
+        $('#curPages').append('<div class="number number_' + i + '" style="display:none;"><a href="javascript:void(0)" onclick="changePageLong(' + (i + 1) + ',false); return false;" >' + (i + 1) + '</a></div> ');
+
+        images += '<img class="unloaded" onClick="nextChap();return false;" style="display:block;" src="/templates/proxer14/images/bg60.png" id="chapterImage' + i + '" width="' + Math.min(pages[i][2], $('#reader').width()) + '" height="' +
+                Math.round(Math.min(pages[i][2], $('#reader').width()) * pages[i][1] / pages[i][2]) + '" />';
+    }
+    list += '</ul>';
+    $('#allPages').html(list);
+    $('#reader').html('<a href="javascript:void(0);">' + images + '</a>');
+
+    create_message('key_suggestion', 7000, gt_key_suggestion_long);
+
+    changePageLong(current_page, true);
+
+    if (nextChapter === '/mcp?section=manga&s=updatesnew#top') {
+        $('#panel').attr('style', 'position:fixed;top:42px;left:50%;width:' + ($('#reader').width() - 10) + 'px;margin-left:-' + ($('#reader').width() / 2) + 'px;');
+    }
+
+    var button_down = false;
+    var button_down_code;
+    jQuery(document).off('keydown');
+    jQuery(document).on('keydown', function (e) {
+        if (!button_down && !jQuery("input").is(":focus") && !jQuery("textarea").is(":focus") && jQuery('#reader').length > 0) {
+            button_down = true;
+            code = e.keyCode || e.which;
+            var windowheight = $(window).height() - 50;
+            if (e.keyCode === 37 || e.keyCode === 65)
+            {
+                if (e.timeStamp - timeStamp37 < 250 && e.timeStamp - timeStamp37 > 100)
+                    thisChap();
+                timeStamp37 = e.timeStamp;
+                $(window).scrollTo("-=" + windowheight);
+                button_down = true;
+                e.preventDefault();
+                button_down_code = setInterval(function () {
+                    if (button_down) {
+                        $(window).scrollTo("-=" + windowheight);
+                    }
+                }, 200);
+            }
+            if (e.keyCode === 39 || e.keyCode === 68)
+            {
+                if (e.timeStamp - timeStamp39 < 250 && e.timeStamp - timeStamp39 > 100)
+                    nextChap();
+                timeStamp39 = e.timeStamp;
+                $(window).scrollTo("+=" + windowheight);
+                if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+                    setTimeout(nextChap, 1500);
+                }
+                button_down = true;
+                e.preventDefault();
+                button_down_code = setInterval(function () {
+                    if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+                        setTimeout(nextChap, 1500);
+                    }
+                    if (button_down) {
+                        $(window).scrollTo("+=" + windowheight);
+                    }
+                }, 200);
+            }
+
+
+            if (code === 40 || code === 83)
+            {
+                e.preventDefault();
+                button_down_code = setInterval(function () {
+                    if (button_down) {
+                        jQuery.scrollTo("+=13");
+                    }
+                }, 20);
+            }
+
+            if (code === 38 || code === 87)
+            {
+                e.preventDefault();
+                button_down_code = setInterval(function () {
+                    if (button_down) {
+                        jQuery.scrollTo("-=13");
+                    }
+                }, 20);
+
+            }
+        }
+
+    });
+    jQuery(document).keyup(function (e) {
+        button_down_code = window.clearInterval(button_down_code);
+        button_down = false;
+    });
+
+    $(window).off('scroll');
+    $(window).on('scroll', function (e) {
+        var loop = true;
+        while (loop) {
+            var position = $('#chapterImage' + (current_page - 1)).position().top;
+            var scrolled = $(window).scrollTop();
+            var curHeight = $('#chapterImage' + (current_page - 1)).height();
+            var prevHeight = 10000;
+            if (current_page - 2 >= 0) {
+                prevHeight = $('#chapterImage' + (current_page - 2)).height()
+            }
+
+            if (scrollable && (scrolled - position) >= curHeight) {
+                scroll = false;
+                nextPageLong();
+            } else if (scrollable && (position - scrolled) >= prevHeight) {
+                scroll = false;
+                prevPageLong();
+            } else {
+                loop = false;
+            }
+        }
+
+        if (!scrollable && counter > 1) {
+            scrollable = true;
+        }
+        counter++;
+    });
+
+    timeStamp37 = 0;
+    timeStamp39 = 0;
+};
+
+function changePageLong(page, init) {
+    if (pages.length === 0) {
+        alert("Fehler beim Laden des Kapitels. Bitte aktualisiere diese Seite.");
+    } else if (page > pages.length) {
+        $('#wrapper').css('max-width', '');
+        //ajaxProxerRequest(nextChapter);
+        window.location = nextChapter + '#top';
+    } else if (page > 0) {
+        var p = page - 1;
+        var preLoad = new Array();
+        for (i = 0; i < pages.length; i++) {
+
+            if ((i < p && i + 3 > p) || (i > p && i - 3 < p) || i === p) {
+                $('.number_' + i).css('display', '');
+                if (i === p) {
+                    $('.number_' + i).addClass('current_page');
+                } else {
+                    $('.number_' + i).removeClass('current_page');
+                }
+                preLoad.push(new Array(i, phpUrldecode(pages[i][0])));
+            } else {
+                if ((i > p && i - 6 < p)) {
+                    preLoad.push(new Array(i, phpUrldecode(pages[i][0])));
+                }
+                $('.number_' + i).css('display', 'none');
+            }
+        }
+        if (scroll) {
+            //$.scrollTo(jQuery('#chapterImage'+(page-1)).position().top-50,0);
+            //$(document).scrollTop($('#chapterImage'+(page-1)).offset().top-50);
+            if (page - 1 !== 0) {
+                window.scrollTo(0, $('#chapterImage' + (page - 1)).offset().top - 50);
+                setTimeout(function () {
+                    window.scrollTo(0, $('#chapterImage' + (page - 1)).offset().top - 50);
+                }, 80);
+            } else {
+                var amount = 0;
+                var tmode = get_cookie('tmode');
+                if (tmode === 'ht' || tmode === '') {
+                    amount = 300;
+                }
+                window.scrollTo(0, amount);
+            }
+        }
+        scroll = true;
+
+        current_page = page;
+        imagePreloadLong(preLoad);
+
+        for (i = 0; i < preLoad.length; i++) {
+            var pre = preLoad[i][0];
+            if ($('#chapterImage' + pre).hasClass("unloaded")) {
+                $('#chapterImage' + pre).removeClass("unloaded");
+                $('#chapterImage' + pre).css('opacity', '1');
+                $('#chapterImage' + pre).attr('src', preLoad[i][1]);
+            }
+        }
+    } else {
+        changePageLong(1, false);
+    }
+}
+
+function thisChap() {
+    $('#wrapper').css('max-width', '');
+    //ajaxProxerRequest(nextChapter);
+    window.location = window.location.href.split('chapter')[1] + '#top';
+}
+
+function nextChap() {
+    $('#wrapper').css('max-width', '');
+    //ajaxProxerRequest(nextChapter);
+    window.location = nextChapter + '#top';
+}
+
+function nextPageLong() {
+    changePageLong(current_page + 1, false);
+}
+
+function prevPageLong() {
+    changePageLong(current_page - 1, false);
+}
+
+function imagePreloadLong(loadPages) {
+    var pageIndex = new Array();
+    var pageImages = new Array();
+    for (i = 0; i < loadPages.length; i++) {
+        pageIndex.push(loadPages[i][0]);
+        pageImages.push(loadPages[i][1]);
+    }
+    jQuery.preload(pageImages, {
+        threshold: 40,
+        enforceCache: true,
+        onComplete: function (data)
+        {
+            var idx = data.index;
+
+            $('.number_' + pageIndex[idx]).addClass('loaded');
+            if (pageIndex[idx] + 1 === current_page) {
+                //$('#chapterImage').animate({'opacity':'1.0'}, 800);
+            }
+        }
+
+    });
+}
---
+++ public_html/Naver2Proxer.user.js
@@ -0,0 +1,118 @@
+// ==UserScript==
+// @name        Naver2Proxer
+// @author      Dravorle
+// @description Ermöglicht es weiterhin auch lizenzierte Naver-Manga direkt in Proxer zu lesen
+// @include     https://proxer.me/chapter/*
+// @supportURL  
+// @updateURL   
+// @version     1.0: First Release
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require     https://proxer.me/templates/proxer14/js/jquery-ui-1.10.3.custom.min.js
+// @require     https://proxer.me/templates/proxer14/js/jquery.plugins.js?3
+// @connect     webtoons.com
+// @connect     webtoon-phinf.pstatic.net
+// @grant       GM_xmlhttpRequest
+// @namespace   dravorle.proxer.me
+// @run-at      document-end
+// ==/UserScript==
+
+run();
+$(document).ajaxSuccess( function() {
+    run();
+});
+
+function run() {
+    /*
+    $('[data-ajax="true"]').on('click', function(){
+        setTimeout(run, 1500);
+    });
+    */
+    
+    if( $(".inner").text().indexOf("Dieses Kapitel ist leider noch nicht verfügbar :/") > -1 ) {
+        console.log( "[Naver2Proxer] Kein Chapter verfügbar." );
+        return;
+    }
+
+    //Prüfen ob das Chapter ein offizielles Webtoons-Chapter ist
+    if( $("#chapter_next").attr("href").indexOf("webtoons.com") > -1 ) {
+        console.log( "[Naver2Proxer] Offizielles Chapter entdeckt." );
+        //Funktion des Links verändern, bei OnClick Webtoons-Seite laden und in Proxer-Style auf der Website anzeigen
+        $("<script> pages = []; baseurl = '"+getCurrentLink().split("?")[0]+"'; current_page = 1; serverurl = ''; nextChapter = '"+$("a.menu:contains('Nächstes Kapitel')").attr("href")+"'; </script>").appendTo("head");
+        $('<script type="text/javascript" src="https://dravorle.de/js/CustomSlide.js"></script>').appendTo("head");
+        $('<script type="text/javascript" src="https://dravorle.de/js/CustomLongstrip.js"></script>').appendTo("head");
+
+        $("#chapter_next").on("click", handleNaverClick );
+        
+        if( location.href.indexOf("?startRead") > -1 ) {
+            $("#chapter_next").trigger("click");
+            history.pushState(null, null, baseurl);
+        }
+    }
+}
+
+function handleNaverClick(e) {
+    e.preventDefault();
+    
+    $("body").append('<div id="loading" class="customBubble" style="display:inline;"></div>');
+    
+    fetchImages();
+}
+
+function fetchImages() {
+    console.log( "Loading Website" );
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: $("#chapter_next").attr("href"),
+        onload: function(response) {
+            $(response.responseText.trim()).find("#_imageList img").each( function() {
+                pages.push( [ $(this).data("url"), $(this).attr("height"), $(this).attr("width") ] );
+
+                enablePage( $(this).data("url"), $("#chapter_next").attr("href") );
+            });
+
+            buildProxerReader( get_cookie("manga_reader") );
+        }
+    });
+}
+
+function buildProxerReader( style ) {
+    $("#main > *").remove();
+    
+    $("<div id='panel'> <div id='breadcrumb'>Hier könnte ihr Titel stehen</div> <div id='navigation'> <span id='curPages'></span> <span id='pages'></span> <span id='allPages'></span> </div> <div id='readers' style='position:relative;float:right;margin:0 20px;font-size: 20px;'><a style='margin:5px;' class='menu"+( ( style === "slide" )?" active":"" )+"' data-style='slide' title='Standardreader' href='javascript:;'><i class='fa fa-arrows-h'></i></a><a style='margin:5px;' class='menu"+( ( style === "slide" )?"":" active" )+"' data-style='longstrip' title='Longstrip-Reader' href='javascript:;'><i class='fa fa-arrows-v'></i></a> </div> <div style='clear:both'></div> </div> <div id='reader' align='center'> <a id='readerLink' href='javascript:;'> <img id='chapterImage' /> </a </div> ").appendTo("#main");
+    $("#readers a.menu").on("click", setReader);
+    
+    $('<link rel="stylesheet" type="text/css" href="/components/com_proxer/css/read/default.css">').appendTo("#main");
+
+    history.pushState(null, null, window.location.href);
+
+    $("#loading").remove();
+    if( style === "slide" ) {
+        initSlide();
+    } else if( style === "longstrip" ) {
+        initLong();
+    } else {
+        initSlide();
+    }
+}
+
+//Funktion ruft einmalig das Bild auf um es für diese IP freizuschalten, dies wird von Naver benötigt, da das Bild erst dann angezeigt werden kann, wenn es 1x von der Naver-Seite aus aufgerufen wurde
+function enablePage(url, base) {
+    console.log( "Loading Image", url );
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: url,
+        headers: {
+            referer: base,
+            origin: base
+        },
+        onload: function(response) {
+            //Hier eventuell Seite in blob zwischenspeichern, oder so
+        }
+    });
+}
+
+function setReader() {
+    set_cookie('manga_reader', $(this).attr("data-style"), cookie_expire);
+    
+    location.href = getCurrentLink() + "startRead";
+}
